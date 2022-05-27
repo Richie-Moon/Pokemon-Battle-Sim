c30d5ab5fe88ccafc48c1b82890da719729a60ef
Richie Moon <richie.moon1green@gmail.com>
2022-05-27 14:07:51 +1200

    Continue work on pick_move()

--- main.py
+++ main.py
@@ -10,12 +10,30 @@ ember = pokemon_move_class.Move('Ember', 40, 'fire', 'Special')
 
 # TODO Finish asking the user for the moves that they want on their pokemon.
 # TODO List all possible moves if asked.
-def pick_moves(number):
+def pick_moves(pokemon, number: int) -> pokemon_move_class.Move:
     with open('movesets.csv', mode='r') as data:
         reader = list(csv.DictReader(data))
+        moves_list = []
 
         while True:
-            user_move = input(f"Please enter move {number}: ")
+            user_move = input(f"Please enter move {number} (Type 'L' to list "
+                              f"all possible moves. ): ").capitalize().strip()
+            for move in reader:
+                if move['species'] == pokemon:
+                    for i in range(1, 175):
+                        if move[f'move{i}'] != '':
+                            moves_list.append(move[f'move{i}'])
+                else:
+                    print("No Pokemon exists with that name.")
+
+            with open('moves.csv', mode='r') as move_data:
+                moves_reader = list(csv.DictReader(move_data))
+                # TODO have to use this file to get move info.
+                if user_move == 'L':
+                    print(", ".join(moves_list))
+                elif user_move in moves_list:
+                    print(f"Picked {user_move} as move {number}!")
+                    return pokemon_move_class.Move()
 
 
 def pick_pokemon(number) -> pokemon_move_class.Pokemon:
@@ -41,7 +59,6 @@ def pick_pokemon(number) -> pokemon_move_class.Pokemon:
                 else:
                     pass
             print('No Pokemon found. ')
-
 
 users_pokemon = []
 for i in range(1, 7):
