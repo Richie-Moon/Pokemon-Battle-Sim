6a85809e26cfba47868a362b104ae54e171503b5
Richie-Moon <richie.moon1green@gmail.com>
2022-05-27 19:32:22 +1200

    Fixes Error #5 and continues work on battle framework

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import csv
 import damage_calc
 import pokemon_move_class
 
-
 # Create moves for testing
 vine_whip = pokemon_move_class.Move('Vine Whip', 45, 'grass', 'Physical')
 ember = pokemon_move_class.Move('Ember', 40, 'fire', 'Special')
@@ -10,30 +9,36 @@ ember = pokemon_move_class.Move('Ember', 40, 'fire', 'Special')
 
 # TODO Finish asking the user for the moves that they want on their pokemon.
 # TODO List all possible moves if asked.
-def pick_moves(pokemon, number: int) -> pokemon_move_class.Move:
+def pick_moves(pokemon, number: int, used_moves: list) -> pokemon_move_class.Move:
     with open('movesets.csv', mode='r') as data:
         reader = list(csv.DictReader(data))
         moves_list = []
 
         while True:
             user_move = input(f"Please enter move {number} (Type 'L' to list "
-                              f"all possible moves. ): ").capitalize().strip()
+                              f"all possible moves): ").title().strip()
             for move in reader:
                 if move['species'] == pokemon:
                     for i in range(1, 175):
                         if move[f'move{i}'] != '':
-                            moves_list.append(move[f'move{i}'])
-                else:
-                    print("No Pokemon exists with that name.")
+                            moves_list.append(move[f'move{i}'].strip())
 
             with open('moves.csv', mode='r') as move_data:
                 moves_reader = list(csv.DictReader(move_data))
-                # TODO have to use this file to get move info.
+
                 if user_move == 'L':
-                    print(", ".join(moves_list))
-                elif user_move in moves_list:
-                    print(f"Picked {user_move} as move {number}!")
-                    return pokemon_move_class.Move()
+                    print(", ".join(set(moves_list)))
+                elif user_move in moves_list and user_move not in used_moves:
+                    for pokemon_move in moves_reader:
+                        if pokemon_move['name'] == user_move:
+                            print(f"Picked {user_move} as move {number}! \n")
+                            return pokemon_move_class.Move(
+                                pokemon_move['name'],
+                                pokemon_move['power'],
+                                pokemon_move['type'],
+                                pokemon_move['category'])
+                else:
+                    print("That move doesn't exist. ")
 
 
 def pick_pokemon(number) -> pokemon_move_class.Pokemon:
@@ -41,10 +46,21 @@ def pick_pokemon(number) -> pokemon_move_class.Pokemon:
         reader = list(csv.DictReader(data))
 
         while True:
-            user_pokemon = input(f"Pokemon {number}: ").capitalize().strip()
+            user_pokemon = input(f"Pokemon {number}: ").title().strip()
             for pokemon in reader:
                 if pokemon['name'] == user_pokemon:
                     print(f"You chose {pokemon['name']}!")
+
+                    moves = []
+
+                    move1 = pick_moves(pokemon['name'], 1, moves)
+                    moves.append(move1.name)
+                    move2 = pick_moves(pokemon['name'], 2, moves)
+                    moves.append(move2.name)
+                    move3 = pick_moves(pokemon['name'], 3, moves)
+                    moves.append(move3.name)
+                    move4 = pick_moves(pokemon['name'], 4, moves)
+                    moves.append(move4.name)
 
                     return pokemon_move_class.Pokemon(pokemon['name'],
                                                       pokemon['type_1'],
@@ -55,10 +71,12 @@ def pick_pokemon(number) -> pokemon_move_class.Pokemon:
                                                       pokemon['df'],
                                                       pokemon['spdf'],
                                                       pokemon['spd'],
-                                                      pokemon[])
+                                                      move1, move2, move3,
+                                                      move4)
                 else:
                     pass
             print('No Pokemon found. ')
+
 
 users_pokemon = []
 for i in range(1, 7):
